29ca58b920d1889bd44c55317da0de8c554e66e2
reiheng <reiheng@gmail.com>
2015-11-14 09:01:47 +0800

    pure index

--- index.php
+++ index.php
@@ -1,66 +1,7 @@
 ﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php include("dbconnect.php");  ?>
-
-<h2>Index 2015/11/14</h2>
-
-<?php	
-$ss = 0;
-if($_POST['submit'])	
-{
-	$id = $_POST['id'];
-	$name = $_POST['name'];
-	$age = $_POST['age'];
-	$memo = $_POST['memo'];
-	
-	//取得上傳檔案資訊
-    $filename=$_FILES['image']['name'];
-    $tmpname=$_FILES['image']['tmp_name'];
-    $filetype=$_FILES['image']['type'];
-    $filesize=$_FILES['image']['size'];    
-    $file=NULL;
-    
-    if(isset($_FILES['image']['error'])){    
-        if($_FILES['image']['error']==0){                                    
-            $instr = fopen($tmpname,"rb" );
-            $file = addslashes(fread($instr,filesize($tmpname)));        
-        }
-    }
-    if ($file = NULL){
-		$ss=1
-	}
-	else
-	{
-       //新增圖片到資料庫
-                        
-	   pg_query("insert into testtable (id,name,age,memo, img) values ('$id','$name','$age','$memo', '$file')");
-	}
-}
-?> 
-
-<?php
-if ($ss=1){
-	echo "insert error"
-}
-else {
-	$result = pg_query("select * from testtable ") or
-
-	die (pg_error());
-	$xx = pg_fetch_all($result);
-
-	foreach ($xx as $row )
-	{
-		echo "<b>ID:</b>". $row['id']
-		."<br><b>Name:</b>". $row['name']
-		."<br><b>Age:</b>". $row['age']
-		."<br><b>Memo:</b>". $row['memo']
-		."<br><b>Image:</b>". $row['img']
-		."<br><br><br>";
-	}
-	pg_free_result($result);
-}
-?>
 
 <h2><a href="search.php">修改</a></h2>
 <h2><a href="delete.php">刪除</a></h2>
 <h2><a href="insert.php">新增</a></h2>
+<h2><a href="display.php">列表</a></h2>
 </form>
